648f19e9e2bbb90e159652fed4acea252ec54cd4
AmitMirgal <amit.s.mirgal@gmail.com>
2020-10-13 13:22:40 -0400

    revamp output file name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: "radioactive.js",
+    filename: "index.js",
     libraryTarget: "commonjs2",
   },
   module: {
